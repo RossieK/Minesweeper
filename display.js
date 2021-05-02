65b78582606702b976168e2f3cbdd2d65868e0e3
RossieK <rosi.kirova96@gmail.com>
2021-05-02 13:37:21 +0300

    Add mark tiles function

--- display.js
+++ display.js
@@ -1,4 +1,4 @@
-import { createBoard } from "./logic.js";
+import { TILE_STATUSES, createBoard, markTile } from "./logic.js";
 
 const BOARD_SIZE = 10;
 const MINES_NUMBER = 10;
@@ -10,8 +10,26 @@ const minesLeftText = document.querySelector('[data-mine-count]');
 board.forEach(row => {
     row.forEach(tile => {
         boardElement.append(tile.element);
+
+        tile.element.addEventListener('click', () => {
+
+        });
+
+        tile.element.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+            markTile(tile);
+            listMinesLeft();
+        });
     });
 });
 
 boardElement.style.setProperty("--size", BOARD_SIZE);
 minesLeftText.textContent = MINES_NUMBER;
+
+function listMinesLeft() {
+    const markedTilesCount = board.reduce((count, row) => {
+        return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length;
+    }, 0);
+
+    minesLeftText.textContent = MINES_NUMBER - markedTilesCount;
+}
